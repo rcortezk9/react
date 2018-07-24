d0e98e7f1e1b806eef7b1a2bcb4d4589c33f1c6b
Rene Cortez <rcortezk9@gmail.com>
2018-07-23 21:56:02 -0500

    Added App class to extend component Added App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';//component feature
+
+class App extends Component {
+    render( ) {//have to include the return statment
+        return (
+            <div>Coundown Champ, App Component</div>
+        )
+    }
+}
+
+export default App;
