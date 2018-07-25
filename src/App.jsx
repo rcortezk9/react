89f463707c5cdce2054d09415a7495d6637c60e0
Rene Cortez <rcortezk9@gmail.com>
2018-07-24 20:14:49 -0500

    Added struction to the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React, {Component} from 'react';//component feature
 class App extends Component {
     render( ) {//have to include the return statment
         return (
-            <div>Coundown Champ, App Component</div>
+            <div>
+                <div>Countdown to December 25, 2017</div>
+                <div>
+                    <div>14 Days</div>
+                    <div>30 Hours</div>
+                    <div>15 Minutes</div>
+                    <div>20 Seconds</div>
+                </div>
+            </div>
         )
     }
 }
